fe4f7280dab125f9c428be4124b1ca4dd1b63b97
PJS <b_rok9999@mail.ru>
2024-07-23 23:13:21 +0500

    Add ReadMe

--- src/main/java/GB/Seminar_5/CW5/Task3.java
+++ src/main/java/GB/Seminar_5/CW5/Task3.java
@@ -10,5 +10,71 @@
  */
 package GB.Seminar_5.CW5;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Stack;
+
 public class Task3 {
+//    public static void main(String[] args) {
+//        String str = "[a+(1*3)";
+//        LinkedList<Integer> values = new LinkedList<Integer>(getMapStaples(str).values());
+//        int sumValues = 0;
+//        for (int i = 0; i < values.size(); i++) {
+//            sumValues+=values.get(i);
+//        }
+//        if (sumValues % 2 == 0) {
+//            System.out.println(true);
+//        } else {
+//            System.out.println(false);
+//        }
+//    }
+//    public static HashMap getMapStaples (String str) {
+//        HashMap mapStr = new HashMap<>();
+//        String[] arrStr = str.split("");
+//        int count = 0;
+//        for (int i = 0; i < arrStr.length; i++) {
+//            switch (arrStr[i]) {
+//                case "[":
+//                case "(":
+//                case "<":
+//                case "{":
+//                case "]":
+//                case ")":
+//                case ">":
+//                case "}":
+//                    mapStr.put(arrStr[i], count+1);
+//            }
+//        }
+//        return mapStr;
+//    }
+//}
+
+    // второй вариант решения
+    public static void main(String[] args) {
+        Map<Character, Character> pair = new HashMap<>();
+        pair.put('(', ')');
+        pair.put('[', ']');
+        pair.put('{', '}');
+        pair.put('<', '>');
+        Stack<Character> lifo = new Stack<>();
+        String srs = "{a+]}{(d*3)}";
+        System.out.println(conditionPairs(srs, pair, lifo));
+
+
+    }
+
+    private static boolean conditionPairs(String srs, Map<Character, Character> pair, Stack<Character> lifo) {
+        for (char c : srs.toCharArray()) {
+            if (pair.containsKey(c)) {
+                lifo.push(c);
+            } else if (pair.containsValue(c)) {
+                if (lifo.isEmpty() || pair.get(lifo.pop()) != c) {
+                    return false;
+                }
+
+            }
+        }
+        return lifo.isEmpty();
+    }
 }
--- src/main/java/GB/Seminar_5/CW5/Task4.java
+++ src/main/java/GB/Seminar_5/CW5/Task4.java
@@ -5,5 +5,35 @@
  */
 package GB.Seminar_5.CW5;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Task4 {
+    public static void main(String[] args) {
+        Map<Character, Integer> data = new HashMap<>();
+        data.put('I', 1);
+        data.put('V', 5);
+        data.put('X', 10);
+        data.put('L', 50);
+        data.put('C', 100);
+        data.put('D', 500);
+        data.put('M', 1000);
+
+        String rimData = "MMXXII";
+        Integer arabData = data.get(rimData.charAt(rimData.length() - 1));
+// String rimData = "MMXXII";
+
+
+
+        for (int i = 0; i < rimData.length() - 1; i++) {
+            if (data.get(rimData.charAt(i)) > data.get(rimData.charAt(i + 1))) {
+                arabData += data.get(rimData.charAt(i));
+            }
+            else {
+                arabData -= data.get(rimData.charAt(i));
+            }
+
+        }
+        System.out.println(arabData);
+    }
 }
